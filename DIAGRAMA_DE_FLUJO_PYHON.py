0064d5397ea2f81db59e9cce89ff524151c394af
Nymos <andresfelipevelasquezsalinas0@gmail.com>
2025-02-19 10:57:42 -0500

    Ejercicios_#3_clase

---
+++ DIAGRAMA_DE_FLUJO_PYHON.py
@@ -0,0 +1,8 @@
+T1= float(input("Digite el primer tiempo: "))
+T2= float(input("Digite el segundo tiempo: "))
+T3= float(input("Digite el tercer tiempo: "))
+
+suma= T1 + T2 +T3
+promedio= suma / 3
+
+print (f"El promedio es: {promedio} " )
---
+++ Diagrama_De_Flujo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    double t1, t2, t3;
+    cout<< "Ingrese el primer tiempo: ";
+    cin >> t1;
+    cout<< "Ingrese el segundo numero: ";
+    cin >> t2;
+    cout<< "Ingrese el tercer numero: ";
+    cin >> t3;
+    cout << (t1 +t2 + t3) / 3 <<endl;
+
+
+}
